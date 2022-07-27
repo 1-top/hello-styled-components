a880074167e24d9e80940451db2994fbb1c1368f
1-top <best.no.1.top@gmail.com>
2022-07-27 18:26:11 +0900

    :sparkles: props, 확장

---
+++ src/components/button/Button.styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+type StyledButtonProps = {
+  variant?: string;
+}
+
+/**
+ * generic 을 이용하여 props 타입을 정의할 수 있다.
+ */
+export const StyledButton = styled.button<StyledButtonProps>`
+  border: 2px solid #4caf50;
+  background-color: ${(props) => props.variant ? '#fff' : '#4caf50'};
+  color: ${props => props.variant ? '#4caf50' : '#fff'};
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  transition: .5s all ease-out;
+`;
+
+/**
+ * style 을 확장할 수 있다.
+ */
+export const FancyButton = styled(StyledButton)`
+  background-image: linear-gradient(to right, #f6d365 0%, #fda085 100%);
+  border: none;
+`;
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyledButton, FancyButton } from './Button.styles';
+
+export default StyledButton;
+
+export { FancyButton }
